08a0e215482e41fe22f065b48766a51ec787ba6b
Cesarperon <cribeiroperon@gmail.com>
2025-07-23 17:55:18 -0300

    Update Teste_5_lux_com_movimentacao.ino

--- Teste_5_lux_com_movimentacao.ino
+++ Teste_5_lux_com_movimentacao.ino
@@ -14,10 +14,9 @@ const int motorC_PWM = 6;
 const int motorD_EN = 8;  // Trás direita
 const int motorD_PWM = 7;
 
+// === Sensor BH1750 ===
 const unsigned int SDA_15 = 20;
 const unsigned int SCL_15 = 21;
-
-// === Sensor BH1750 ===
 BH1750 lightMeter;
 
 // === Variáveis de movimento ===
@@ -25,6 +24,7 @@ int LeftFront = 0;
 int RightFront = 0;
 int LeftBack = 0;
 int RightBack = 0;
+int velocidade = 0;  // agora é global
 
 // === Setup ===
 void setup() {
@@ -42,39 +42,41 @@ void setup() {
   Wire.setSDA(SDA_15);
   Wire.setSCL(SCL_15);
   Wire.begin();
+}
 
 // === Loop ===
 void loop() {
-  float lux = lightMeter.readLightLevel();
+  velocidadeporlux();  // Atualiza a variável global "velocidade"
 
-  int velocidade = calcularVelocidadePorLux(lux);
-
-// === Função para definir velocidade conforme lux ===
-int calcularVelocidadePorLux(float lux) {
-  if (lux > 150) {
-    return 255;
-  } else if (lux > 100) {
-    return 200;
-  } else if (lux > 50) {
-    return 150;
-  } else {
-    return 100;
-  }
-}
-
-  // Movimento simples para frente
-  
+  // Movimento para frente com a velocidade ajustada
   LeftFront = velocidade;
   RightFront = velocidade;
   LeftBack = velocidade;
   RightBack = velocidade;
 
+  delay(500);
+}
+
+// === Função que ajusta a velocidade com base na luz ===
+void velocidadeporlux() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Lux: ");
+  Serial.println(lux);
+
+  if (lux > 150) {
+    velocidade = 255;
+  } else if (lux > 100) {
+    velocidade = 200;
+  } else if (lux > 50) {
+    velocidade = 150;
+  } else {
+    velocidade = 100;
+  }
+
   setMotor(motorA_EN, motorA_PWM, LeftFront);
   setMotor(motorB_EN, motorB_PWM, RightFront);
   setMotor(motorC_EN, motorC_PWM, LeftBack);
   setMotor(motorD_EN, motorD_PWM, RightBack);
-
-  delay(500);
 }
 
 // === Função para controlar motores com direção ===
